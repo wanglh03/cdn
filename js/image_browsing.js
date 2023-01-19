bebe636eaaa719c764915b6235cd0e49ec273ccd
wangleih <leihaowang2003@gmail.com>
2023-01-19 16:24:23 +0800

    modified image_browsing.js

--- js/image_browsing.js
+++ js/image_browsing.js
@@ -1,38 +1,38 @@
 document.write(`
+    <script src="https://cdn.jsdelivr.net/gh/wanglh03/cdn/js/standard.js"></script>
     <h1>${document.title}</h1>
     <p>
-    <b><a href="./">返回上级目录</a></b>。
-    原图若较大，大图加载可能较慢。
-    &nbsp;&nbsp;<b>缩略图</b>：<img id="thumb" src=""" width=" 100" height="100">
+      <a href="./"><b>返回上级目录</b></a>。原图若较大，大图加载可能较慢。
+      <b>缩略图</b>：<img id="thumb" src="" width="100" height="100">
     </p>
     <div class="box">
-    <button id="minus" onclick="minusContent()">上一页</button>
-    第<input type="text" id="content" value="1" style="width: 60px">页
-    <button id="display-image" onclick="displayImage()">跳转</button>
-    <button id="plus" onclick="plusContent()">下一页</button>
+      <button id="minus" onclick="minusContent()">上一页</button>
+      第<input type="text" id="content" value="1" style="width: 60px">页
+      <button id="display-image" onclick="displayImage()">跳转</button>
+      <button id="plus" onclick="plusContent()">下一页</button>
     </div>
-    <img id="original" src="" onerror=displayImage() > `
+    <img id="original" src="" onerror=displayImage()>`
 );
 
 function plusContent() {
-    let content = document.getElementById('content').value;
+    let content = $('content').value;
     if (content >= maximumPage) {
         showIllegalContent();
     } else {
         content++;
     }
-    document.getElementById('content').value = content;
+    $('content').value = content;
     displayImage();
 }
 
 function minusContent() {
-    let content = document.getElementById('content').value;
+    let content = $('content').value;
     if (content <= 1) {
         showIllegalContent();
     } else {
         content--;
     }
-    document.getElementById('content').value = content;
+    $('content').value = content;
     displayImage();
 }
 
@@ -41,12 +41,12 @@ function showIllegalContent() {
 }
 
 function displayImage() {
-    let content = document.getElementById('content').value;
+    let content = $('content').value;
     if (content < 1 || content > maximumPage) {
         showIllegalContent();
     } else {
         document.images.original.src = "";
-        let content = document.getElementById('content').value;
+        let content = $('content').value;
         let url = ` (${content}).${fileType}`;
         document.images.thumb.src = `${relativeDir}thumb/${url}`;
         document.images.original.src = `${relativeDir}${url}`;
